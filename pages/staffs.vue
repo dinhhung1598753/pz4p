1d5a2358142f1cf60165bd96edd86dc9c18959e2
dinhhung1598753 <hoangdinhhung159@gmail.com>
2022-07-03 00:29:06 +0700

    update main

--- pages/staffs.vue
+++ pages/staffs.vue
@@ -8,7 +8,7 @@
       <template #top>
         <v-toolbar flat color="base">
           <v-toolbar-title class="text-light">STAFFS</v-toolbar-title>
-          <v-divider class="mx-4" inset vertical></v-divider>
+          <v-divider class="mx-4" inset vertical> </v-divider>
           <v-spacer></v-spacer>
           <v-dialog v-model="dialog" max-width="500px">
             <template #activator="{ on, attrs }">
