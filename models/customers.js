5641097975a7815eb58237de419f27320d778ec8
HuuKhiem <huukhiem001@gmail.com>
2022-07-15 22:47:42 +0700

    sua customer

---
+++ models/customers.js
@@ -0,0 +1,13 @@
+import { data } from '@/mocks/customers.json'
+export function getCustomers() {
+  return data
+}
+export function insertCustomer() {
+  return data
+}
+export function updateCustomer() {
+  return data
+}
+export function deleteCustomer() {
+  return data
+}
