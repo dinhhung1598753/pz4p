7a2f64c6838f30c3cd95bcb9dfcc03045e6e740c
HuuKhiem <huukhiem001@gmail.com>
2022-07-30 10:37:32 +0700

    them menu

---
+++ models/menus.js
@@ -0,0 +1,13 @@
+import { data } from '@/mocks/Menus.json'
+export function getMenus() {
+  return data
+}
+export function insertMenu() {
+  return data
+}
+export function updateMenu() {
+  return data
+}
+export function deleteMenu() {
+  return data
+}
---
+++ pages/menus.vue
@@ -0,0 +1,199 @@
+<template>
+  <v-container>
+    <v-data-table
+      :headers="headers"
+      :items="menus"
+      class="elevation-1 app-table"
+    >
+      <template #top>
+        <v-toolbar flat color="base">
+          <v-toolbar-title class="text-light">MENUS</v-toolbar-title>
+          <v-divider class="mx-4" inset vertical></v-divider>
+          <v-spacer></v-spacer>
+          <v-dialog v-model="dialog" max-width="500px">
+            <template #activator="{ on, attrs }">
+              <v-btn dark class="mb-2" v-bind="attrs" v-on="on">
+                New Menu
+              </v-btn>
+            </template>
+            <v-card>
+              <v-card-title>
+                <span class="text-h5">{{ formTitle }}</span>
+              </v-card-title>
+
+              <v-card-text>
+                <v-container>
+                  <v-row>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        v-model="editedItem.name"
+                        label="name"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        v-model="editedItem.phonenumeber"
+                        label="phonenumeber"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        v-model="editedItem.sex"
+                        label="sex"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        v-model="editedItem.address"
+                        label="address"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        v-model="editedItem.email"
+                        label="email"
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="close">
+                  Cancel
+                </v-btn>
+                <v-btn color="blue darken-1" text @click="save"> Save </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+          <v-dialog v-model="dialogDelete" max-width="500px">
+            <v-card>
+              <v-card-title class="text-h5"
+                >Are you sure you want to delete this menu?</v-card-title
+              >
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="closeDelete"
+                  >Cancel</v-btn
+                >
+                <v-btn color="blue darken-1" text @click="deleteItemConfirm"
+                  >OK</v-btn
+                >
+                <v-spacer></v-spacer>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-toolbar>
+      </template>
+      // eslint-disable-next-line vue/valid-v-slot
+      <template #[`item.actions`]="{ item }">
+        <v-icon small class="mr-2" @click="editItem(item)"> mdi-pencil </v-icon>
+        <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+      </template>
+      <template #no-data>
+        <v-btn color="primary" @click="initialize"> Reset </v-btn>
+      </template>
+    </v-data-table>
+  </v-container>
+</template>
+
+<script>
+import { getMenus, insertMenu, updateMenu, deleteMenu } from '@/models'
+export default {
+  name: 'MenuPage',
+  data: () => ({
+    dialog: false,
+    dialogDelete: false,
+    headers: [
+      {
+        text: 'Name',
+        align: 'start',
+        sortable: false,
+        value: 'name',
+      },
+      { text: 'Category', value: 'category' },
+      { text: 'Price', value: 'price' },
+      { text: 'Description', value: 'description' },
+      { text: 'Image', value: 'image' },
+      { text: 'Actions', value: 'actions', sortable: false },
+    ],
+    Menus: [],
+    editedIndex: -1,
+    editedItem: {
+      name: '',
+      category: '',
+      price: '',
+      description: '',
+      image: '',
+    },
+    defaultItem: {
+      name: '',
+      category: '',
+      price: '',
+      description: '',
+      image: '',
+    },
+  }),
+  computed: {
+    formTitle() {
+      return this.editedIndex === -1 ? 'New Menu' : 'Edit Menu'
+    },
+  },
+  watch: {
+    dialog(val) {
+      val || this.close()
+    },
+    dialogDelete(val) {
+      val || this.closeDelete()
+    },
+  },
+  created() {
+    this.initialize()
+  },
+  methods: {
+    initialize() {
+      this.menus = getMenus()
+    },
+    editItem(item) {
+      this.editedIndex = this.menus.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialog = true
+    },
+    deleteItem(item) {
+      this.editedIndex = this.menus.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialogDelete = true
+    },
+    deleteItemConfirm() {
+      deleteMenu(this.editedItem.id)
+      this.menus.splice(this.editedIndex, 1)
+      this.closeDelete()
+    },
+    close() {
+      this.dialog = false
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      })
+    },
+    closeDelete() {
+      this.dialogDelete = false
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      })
+    },
+    save() {
+      if (this.editedIndex > -1) {
+        updateMenu(this.editedItem)
+        Object.assign(this.menus[this.editedIndex], this.editedItem)
+      } else {
+        insertMenu(this.editedItem)
+        this.menus.push(this.editedItem)
+      }
+      this.close()
+    },
+  },
+}
+</script>
